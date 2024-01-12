f10a71406e07468506191876e513d227cbc97119
Marat Ayaibergenov <marataya@gmail.com>
2024-01-12 21:47:17 +0600

    add: naive solution to longest_pal_substr

--- longest_palindromic_substr.py
+++ longest_palindromic_substr.py
@@ -155,8 +155,49 @@ def manacherLongestPalindromeSubstr(seq: str) -> str:
 '''
 O(n^2) solution using
 '''
-# def naiveLongestPalindromeSubstr(s: str) -> str:
-#     seqLen = len(seq)
+def naiveLongestPalindromes(seq):
+    """
+    Given a sequence seq, returns a list l such that l[2 * i + 1]
+    holds the length of the longest palindrome centered at seq[i]
+    (which must be odd), l[2 * i] holds the length of the longest
+    palindrome centered between seq[i - 1] and seq[i] (which must be
+    even), and l[2 * len(seq)] holds the length of the longest
+    palindrome centered past the last element of seq (which must be 0,
+    as is l[0]).
+
+    The actual palindrome for l[i] is seq[s:(s + l[i])] where s is i
+    // 2 - l[i] // 2. (// is integer division.)
+
+    Example:
+    naiveLongestPalindrome('ababa') -> [0, 1, 0, 3, 0, 5, 0, 3, 0, 1]
+
+    Runs in quadratic time.
+    """
+    seqLen = len(seq)
+    lLen = 2 * seqLen + 1
+    l = []
+
+    for i in range(lLen):
+        # If i is even (i.e., we're on a space), this will produce e
+        # == s.  Otherwise, we're on an element and e == s + 1, as a
+        # single letter is trivially a palindrome.
+        s = i // 2
+        e = s + i % 2
+
+        # Loop invariant: seq[s:e] is a palindrome.
+        while s > 0 and e < seqLen and seq[s - 1] == seq[e]:
+            s -= 1
+            e += 1
+
+        l.append(e - s)
+    max = 0
+    max_i = 0
+    for i in range(len(l)):
+        if max < l[i]:
+            max = l[i]
+            max_i = i
+    resultStr = interleave(seq)
+    return resultStr[max_i - max : max_i + max+1].replace('#', '')
 
 if __name__ == '__main__':
     # begin = time()
@@ -167,6 +208,12 @@ if __name__ == '__main__':
 
     begin = time()
     print(manacherLongestPalindromeSubstr("cvvd"))
+
+    end = time()
+    print(f'Brute-force: {(end - begin) * 1000} ms')
+
+    begin = time()
+    print(naiveLongestPalindromes("cvvd"))
 
     end = time()
     print(f'Brute-force: {(end - begin) * 1000} ms')
