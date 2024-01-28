a2b83a8a26a8317d997546a9ca0ea858ee6b4b0b
Marat Ayaibergenov <marataya@gmail.com>
2024-01-28 23:01:09 +0600

    add: Queue based on linked list linked_queue.py

--- linked_list/linked_queue.py
+++ linked_list/linked_queue.py
@@ -1,18 +1,16 @@
 import sys
 from exceptions import Empty
-class LinkedStack:
-    # ---------------------------Nested Node class---------------------
+class LinkedQueue:
     class Node:
         __slots__ = '_element', '_next'
-
         def __init__(self, element, next):
             self._element = element
             self._next = next
 
-#      ----------------------Stack methods----------------------------
 
     def __init__(self):
         self._head  = None
+        self._tail  = None
         self._size = 0
 
     def __len__(self):
@@ -25,25 +23,37 @@ class LinkedStack:
         self._head = self.Node(e, self._head) # create and link a new node
         self._size += 1
 
-    def top(self):
+    def first(self):
         '''return but not remove the element at the top of the stack'''
         if self.is_empty():
             raise Empty('Stack is empty')
         return self._head._element
 
-    def pop(self):
+    def dequeue(self):
         if self.is_empty():
             raise Empty('Stack is empty')
         answer = self._head._element
         self._head = self._head._next
         self._size -= 1
+        # if remove node was single then it was the tail and tail should be set None
+        if self.is_empty():
+            self._tail = None
         return answer
 
-if __name__ == '__main__':
-    stack = LinkedStack()
-    stack.push(5)
-    stack.push(7)
-    stack.push(9)
-    print(stack.top())
-    print(len(stack))
+    def enqueue(self, e):
+        newest = self.Node(e, None)
+        if self.is_empty():
+            self._head = newest
+        else:
+            self._tail._next = newest
+        self._tail = newest
+        self._size += 1
 
+
+if __name__ == '__main__':
+    queue = LinkedQueue()
+    print(queue._head, queue._tail)
+    queue.enqueue(5)
+    print(queue._head._element, queue._tail._element)
+    queue.enqueue(7)
+    print(queue._head._element, queue._tail._element)
