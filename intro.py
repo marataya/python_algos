81f88640456ae97db193f55fce4ed6abda8ff0ec
Marat Ayaibergenov <marataya@gmail.com>
2024-02-07 04:28:33 +0600

    add: introduction to algos showing time difference between insert and append

---
+++ intro.py
@@ -0,0 +1,22 @@
+from time import time
+"""inserting 1st element and appending with reversing time """
+if __name__ == '__main__':
+    count = 10 ** 5
+    nums = []
+    begin = time()
+    for i in range(count):
+        nums.append(i)
+    nums.reverse()
+    end = time()
+    diff1 = end-begin
+    print(f'{diff1} sec')
+    nums = []
+    begin = time()
+    for i in range(count):
+        nums.insert(0, i)
+    nums.reverse()
+    end = time()
+    diff2 = end-begin
+    print(f'{diff2} sec')
+    print(f'2nd algo is {diff2/diff1:0.2f} times slower')
+
