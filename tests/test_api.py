d0eddbb86e0df165189ea6ad8d462d45b2eb68b2
Marat Ayaibergenov <marataya@gmail.com>
2024-04-01 09:49:07 +0500

    Add API and UI tests

---
+++ tests/test_api.py
@@ -0,0 +1,12 @@
+import pytest
+import requests
+
+# @pytest.mark.api
+def test_duckduckgo_instant_answer_api():
+    url = "https://api.duckduckgo.com/?q=python+programming&format=json"
+
+    response = requests.get(url)
+    body = response.json()
+
+    assert response.status_code == 200
+    assert 'Python' in body['AbstractText']
